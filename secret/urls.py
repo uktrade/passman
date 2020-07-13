dfbac8d67d65f61f08ea0767659a783100f6669d
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2020-07-13 10:48:58 +0100

    fix urls

--- secret/urls.py
+++ secret/urls.py
@@ -6,6 +6,6 @@ app_name = 'secret'
 
 urlpatterns = [
     path('', views.SecretListView.as_view(), name='list'),
-    path('<str:pk>', views.SecretDetailView.as_view(), name='detail'),
+    path('secret/<str:pk>/', views.SecretDetailView.as_view(), name='detail'),
     path('create/', views.SecretCreateView.as_view(), name='create'),
 ]
