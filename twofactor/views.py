f7ba05ab69421faec316f739b257e619687ff5f8
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2020-07-25 15:09:20 +0100

    sanitise 2fa verify next url

--- twofactor/views.py
+++ twofactor/views.py
@@ -1,7 +1,10 @@
 from functools import partial
 import io
 
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.views import SuccessURLAllowedHostsMixin
 from django.contrib import messages
+from django.utils.http import url_has_allowed_host_and_scheme
 from django.urls import reverse_lazy
 from django.views.generic import FormView
 
@@ -49,7 +52,7 @@ class TwoFactorEnrollView(FormView):
         context['device_confirmed'] = device.confirmed
 
         if not device.confirmed:
-            context['qrcode'] = self.make_qrcode()
+            context['qrcode'] = self.make_qrcode(device)
 
         return context
 
@@ -64,10 +67,11 @@ class TwoFactorEnrollView(FormView):
         return super().form_valid(form)
 
 
-class TwoFactorVerifyView(FormView):
+class TwoFactorVerifyView(SuccessURLAllowedHostsMixin, FormView):
     form_class = OTPTokenForm
     template_name = 'twofactor/verify.html'
     success_url = reverse_lazy('secret:list')
+    redirect_field_name = REDIRECT_FIELD_NAME
 
     def get_form_class(self):
         return partial(self.form_class, self.request.user)
@@ -77,9 +81,21 @@ class TwoFactorVerifyView(FormView):
 
         return super().form_valid(form)
 
-    def get_success_url(self):
-        if 'next' in self.request.GET:
-            # TODO: make sure this is validated safe
-            return self.request.GET['next']
+    def get_redirect_url(self):
+        """Return the user-originating redirect URL if it's safe."""
+        redirect_to = self.request.POST.get(
+            self.redirect_field_name,
+            self.request.GET.get(self.redirect_field_name, '')
+        )
+        url_is_safe = url_has_allowed_host_and_scheme(
+            url=redirect_to,
+            allowed_hosts=self.get_success_url_allowed_hosts(),
+            require_https=self.request.is_secure(),
+        )
+        return redirect_to if url_is_safe else ''
 
-        return super().get_success_urL()
+    def get_success_url(self):
+
+        url = self.get_redirect_url()
+
+        return url or self.success_url
