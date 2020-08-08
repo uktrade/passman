3d509028434bd001d5977b76b872d5e20d5622bc
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2020-08-08 17:23:31 +0100

    Add search to the user/group selects on permissios page

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticfiles"),
+]
 
 AUTH_USER_MODEL = "user.User"
 
