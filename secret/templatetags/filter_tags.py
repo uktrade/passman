535cce93515502f0e24b63a678722dcaec52d8d5
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2020-08-11 17:54:14 +0100

    remember existing group/filter/query options when paging

--- secret/templatetags/filter_tags.py
+++ secret/templatetags/filter_tags.py
@@ -8,10 +8,17 @@ register = template.Library()
 FILTER_PARAM = "o"
 
 
+VALID_QS_PARAMS = ["group", "name", "page", "me", "o"]
+
+
+def extact_qs_params(request):
+    return {name: value for name, value in request.GET.items() if name in VALID_QS_PARAMS}
+
+
 @register.simple_tag(takes_context=True)
-def filter_ordering_qs(context, field_name):
+def filter_querystring(context, field_name):
     request = context["request"]
-    qs_params = {name: value for name, value in request.GET.items()}
+    qs_params = extact_qs_params(request)
 
     filter = qs_params.get(FILTER_PARAM, "")
 
@@ -21,5 +28,15 @@ def filter_ordering_qs(context, field_name):
         filter = f"-{field_name}"
 
     qs_params[FILTER_PARAM] = filter
+
+    return "?" + urlencode(qs_params)
+
+
+@register.simple_tag(takes_context=True)
+def page_querystring(context, page_number):
+    request = context["request"]
+    qs_params = extact_qs_params(request)
+
+    qs_params["page"] = str(page_number)
 
     return "?" + urlencode(qs_params)
