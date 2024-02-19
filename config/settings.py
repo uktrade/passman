579beb86523886fe805020060be57f5688ddb727
Adam Wozencroft <adamwozencroft@DBT000516.local>
2024-02-19 14:06:05 +0000

    Remove csrf_trusted_origins environment variable from settings.py

--- config/settings.py
+++ config/settings.py
@@ -252,7 +252,4 @@ LOGGING = {
     },
 }
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://passman.staging-ci.uktrade.digital',
-    'https://passman.ci.uktrade.digital'
-]
+CSRF_TRUSTED_ORIGINS = env("CSRF_TRUSTED_ORIGINS").split(",")
